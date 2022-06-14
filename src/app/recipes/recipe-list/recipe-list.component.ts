55934cc02f32b6e295e513022c23cdaab90416db
acoss22 <anacseq@gmail.com>
2022-06-14 13:03:21 +0100

    feat: replace event emmiter with service

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,32 +1,20 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
 import { Recipe } from '../recipe.model';
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.scss'],
+  styleUrls: ['./recipe-list.component.scss']
 })
 export class RecipeListComponent implements OnInit {
-  @Output() recipeWasSelected = new EventEmitter<Recipe>();
+  recipes: Recipe[];
 
-  recipes: Recipe[] = [
-    new Recipe(
-      'Cod with cream',
-      'this is a cod fish recipe with cream on top',
-      '/assets/cod.jpg'
-    ),
-    new Recipe(
-      'Cod to Braz',
-      'this is a cod fish recipe with fried potato',
-      '/assets/codtobraz.jpg'
-    )
-  ];
-  constructor() {}
+  constructor(private recipeService: RecipeService) {
+  }
 
-  ngOnInit(): void {}
-
-  onRecipeSelected(recipe: Recipe) {
-    this.recipeWasSelected.emit(recipe);
-   }
- 
+  ngOnInit() {
+    this.recipes = this.recipeService.getRecipes();
+  }
 }
